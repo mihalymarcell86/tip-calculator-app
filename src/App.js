da4f8647fd615e0cbf46ea949d90b64375454e73
Marcell Mihály <mihalymarcell86@gmail.com>
2022-06-12 04:14:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount /
+      person Total / person Reset
+    </>
   );
 }
-
-export default App;
---
+++ src/components/Attribution.js
@@ -0,0 +1,16 @@
+export default function Attribution() {
+  return (
+    <div class="attribution">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by <a href="https://github.com/mihalymarcell86">Marcell Mihály</a>
+      .
+    </div>
+  );
+}
